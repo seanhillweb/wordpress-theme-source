8fe8c7f72e7a7d8c54035d8aaec2c6e4e8a8a324
Sean Hill <seanhillweb@gmail.com>
2016-04-28 16:13:17 -0500

    trailing spaces fix

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form class="search-form" action="<?php echo home_url( '/' ); ?>" method="get" role="search">
-	
-    <div>
+
+	<div>
 		<input type="search" class="search-field" placeholder="Search" value="" name="s">
 		<input type="submit" class="search-submit" value="&#xf002;">
 	</div>
-        
+
 </form>
