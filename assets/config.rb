31db29ae1ee3faf316d6f8ceabde578595f7c61d
Sean Hill <seanhillweb@gmail.com>
2016-04-28 15:58:05 -0500

    Edits to comments and directions

--- assets/config.rb
+++ assets/config.rb
@@ -1,12 +1,11 @@
 # Compass is a great cross-platform tool for compiling SASS. 
-# This compass config file will allow you to 
-# quickly dive right in.
+# This compass config file will allow you to quickly dive right in.
 # For more info about compass + SASS: http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
 # 1. Set this to the root of your project when deployed:
 http_path = "/"
 
-# 2. probably don't need to touch these
+# 2. Set the working directories for your project:
 css_dir = "css"
 sass_dir = "scss"
 fonts_dir = "fonts"
@@ -15,15 +14,14 @@ javascripts_dir = "js"
 environment = :development
 relative_assets = true
 
-
-# 3. You can select your preferred output style here (can be overridden via the command line):
+# 3. Select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
 
-# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
+# 4. When you are ready to launch your Wordpress theme, comment out the 'output_style' above, and then uncomment the 'output_style' below:
 # output_style = :compressed
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+# To disable debugging comments that display the original location of your selectors. Uncomment the line below:
 line_comments = false
 
-# don't touch this
+# Set preferred_syntax (duh):
 preferred_syntax = :scss
