c7c2a6a9fc65fa7fac1333388be2a7e88fab2ce6
Sean Hill <seanhillweb@gmail.com>
2016-05-09 17:02:32 -0500

    add custom post to wordpress taxonomies function

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -126,4 +126,18 @@ register_taxonomy( 'theme_custom_tag',
 	)
 );
 
+// Add custom post type to default Wordpress taxonomies
+function theme_add_custom_types_to_tax( $query ) {
+	if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+	// Get all your post types
+	$post_types = get_post_types();
+
+	$query->set( 'post_type', $post_types );
+		return $query;
+	}
+}
+
+add_filter( 'pre_get_posts', 'theme_add_custom_types_to_tax' );
+
 ?>
